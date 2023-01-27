4001954c8881be2f7974e80c02b08291853e5eaf
Sean <balayans2014@yahoo.com>
2023-01-27 12:12:30 -0800

    wip

--- lib/models.py
+++ lib/models.py
@@ -31,8 +31,21 @@ class Review(Base):
     comment = Column(String())
     
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default = func.now())
+    updated_at = Column(DateTime(), onupdate = func.now())
+    reviews = relationship('Review', backref='user')
+
+    def __repr__(self):
+        return f'User(id={self.id}, name={self.name})'
